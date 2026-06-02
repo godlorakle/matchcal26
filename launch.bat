3a46ff619c2eae865a80d468ca35cb8ed4244c14
godlorakle <nicolas.fintzel@gmail.com>
2026-06-02 20:29:44 +0200

    fix: use port 5026 to avoid conflicts

--- launch.bat
+++ launch.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Starting World Cup 2026 Calendar...
-start /b python -m http.server 7822 --directory "%~dp0"
+start /b python -m http.server 5026 --directory "%~dp0"
 timeout /t 2 /nobreak > nul
-start "" "http://localhost:7822/worldcup2026.html"
+start "" "http://localhost:5026/worldcup2026.html"
 echo.
-echo Server running at http://localhost:7822
+echo Server running at http://localhost:5026
 echo Close this window to stop the server.
 pause > nul
